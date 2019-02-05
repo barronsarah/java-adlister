7bc432c0e16196f0bf4b83f2512518e6a36456ae
Sarah Barron <s.barron2338@gmail.com>
2019-02-05 10:29:18 -0600

    eod hello world and counter

---
+++ src/main/java/CounterServlet.java
@@ -0,0 +1,25 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+  @WebServlet(name = "CounterServlet", urlPatterns = "/counter")
+
+public class CounterServlet extends HttpServlet {
+  private int counter = 0;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+      String reset = request.getParameter("reset");
+      if(reset == null) {
+        counter +=1;
+      } else {
+        counter = 0;
+      }
+    response.getWriter().println("<h1>The count is " + counter + ".</h1>");
+    }
+  }
--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -5,15 +5,26 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-  @WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
+//  the name in the servlet NEEDS to be the same as the class name
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
   public class HelloWorldServlet extends HttpServlet {
 
   @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
       response.setContentType("text/html");
       PrintWriter out = response.getWriter();
+
+//      ASSIGNMENT 1:
+//   Make the page say "Hello, <name>!" if name is passed as part of the query string (e.g. /hello?name=codeup), otherwise default to "Hello, World!".
+
+    String name = request.getParameter("name");
+
+    if(name == null){
+      name = "World";
+    }
       out.println("<h1>Hello, World!</h1>");
             }
+
 
 
     }
